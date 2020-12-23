df4666467d14b4c11519545b5be49889921fc739
clarelo15 <68872705+clarelo15@users.noreply.github.com>
2020-12-22 23:37:39 -0500

    Update app.js
    update port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb+srv://admin-clare:Test123@cluster0.hokfw.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', true);
 
 const itemsSchema = {
